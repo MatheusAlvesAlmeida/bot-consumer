be9b844cc354dff012885b07cec1b993fa801826
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2022-05-27 13:15:23 -0300

    imports necessarios e ajustes finais

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-
 import { SuggestionsListComponent } from './suggestions-list/suggestions-list.component';
+import { AppFacade } from './app-facade';
+import { SuggestionAPI } from 'src/api/suggestions-api';
+import { UsersAPI } from 'src/api/users-api';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, SuggestionsListComponent],
@@ -22,8 +23,9 @@ import { SuggestionsListComponent } from './suggestions-list/suggestions-list.co
     MatDividerModule,
     MatListModule,
     MatIconModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [AppFacade, SuggestionAPI, UsersAPI],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- frontend/src/app/suggestions-list/suggestions-list.component.ts
+++ frontend/src/app/suggestions-list/suggestions-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppFacade } from '../app-facade';
 import { userModel } from 'src/models/user-model';
 import { suggestionModel } from 'src/models/suggestion-model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-suggestions-list',
@@ -9,7 +10,15 @@ import { suggestionModel } from 'src/models/suggestion-model';
   styleUrls: ['./suggestions-list.component.css'],
 })
 export class SuggestionsListComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly appFacade: AppFacade) {}
 
-  ngOnInit(): void {}
+  users: Observable<userModel[]> | undefined;
+  allSuggestions = [];
+
+  ngOnInit(): void {
+    const allUsers = this.appFacade.getAllUsers();
+    allUsers.subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
